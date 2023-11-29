68f8feb04e46ab3a4b4cf3d74dab49416ac92869
Gitkerus <91567981+Gitkerus@users.noreply.github.com>
2023-11-29 13:49:46 +0400

    testing scroll event on navbar

--- src/components/wrappers/ObserverWrap/ObserverWrap.jsx
+++ src/components/wrappers/ObserverWrap/ObserverWrap.jsx
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import { useGlobalContext } from "../../../context/contex";
 import classnames from "classnames";
 
 const ObserverWrap = ({ children, id, className }) => {
   const { setVisibleContent } = useGlobalContext();
+  const sections = useRef([]);
 
-  const { ref } = useInView({
-    threshold: 0.5,
-    rootMargin: "",
-    onChange: (inView, entry) => {
-      if (inView) {
-        setVisibleContent(entry.target.id);
+  const handleScroll = () => {
+    const pageYOffset = window.scrollY;
+    let newVisibleContent = null;
+
+    sections.current.forEach((section) => {
+      const sectionOffsetTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (
+        pageYOffset >= sectionOffsetTop - 48 &&
+        pageYOffset < sectionOffsetTop + sectionHeight - 48
+      ) {
+        newVisibleContent = section.id;
       }
-    },
-  });
+    });
+
+    setVisibleContent(newVisibleContent);
+  };
+
+  useEffect(() => {
+    sections.current = document.querySelectorAll(".contentBlock");
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // const { ref } = useInView({
+  //   threshold: 0.5,
+  //   rootMargin: "",
+  //   onChange: (inView, entry) => {
+  //     if (inView) {
+  //       setVisibleContent(entry.target.id);
+  //     }
+  //   },
+  // });
   return (
-    <div className={classnames(className, "contentBlock")} id={id} ref={ref}>
+    <div className={classnames(className, "contentBlock")} id={id}>
       {children}
     </div>
   );
